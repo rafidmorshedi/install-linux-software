d04aa66eab00bd6f2fcc0e13149242d1e82e80a2
Rafid Morshedi <rafid.morshedi@gmail.com>
2018-03-10 16:31:41 +1100

    Added QGIS repo, removed simple note

--- install.sh
+++ install.sh
@@ -4,13 +4,21 @@ sudo apt-get update && sudo apt-get upgrade
 #Install redshift and a few other tools
 sudo apt-get -y install redshift gnome-tweak-tool gufw gnucash
 
+#install okular
+sudo apt-get -y install okular
+
 #install Ruby and Jekyll (for working on blogs and the SUSS website)
 sudo apt-get -y install ruby-full
 sudo gem install jekyll
 
 # Maps and GIS software
-sudo apt-get -y install grass qgis gdal-bin \
+sudo apt-get -y install grass gdal-bin \
 proj-bin proj-data gmt gmt-doc gpsbabel gpsprune
+
+# install qgis
+sudo add-apt-repository ppa:ubuntugis/ppa
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get -y install qgis
 
 # Data processing (most of the required data software will be installed thorugh Anaconda)
 sudo apt-get -y install sqlite sqlite3 sqliteman octave
@@ -46,27 +54,7 @@ sensible-browser https://www.google.com/chrome/browser/desktop/index.html
 
 #Pauses installs until atom has been installed by the used
 echo "Go ahead and install Chrome"
-read -p "Press [Enter] key to move onto installing Skype"
+read -p "Press [Enter] key to move onto installing Anaconda (Python)"
 
-#install skype
-cd /tmp
-sensible-browser https://www.skype.com/en/download-skype/skype-for-linux/
-
-#Pauses installs until atom has been installed by the used
-echo "Go ahead and install Skype"
-read -p "Press [Enter] key to move onto installing Anaconda"
-
-#download the anaconda install shell (Python 2.7 version) and run the script
-cd /tmp
-wget https://repo.continuum.io/archive/Anaconda2-4.4.0-Linux-x86_64.sh
-bash Anaconda2-4.4.0-Linux-x86_64.sh
-
-echo "Go ahead and install Chrome"
-read -p "Press [Enter] key to move onto installing Simplenote"
-
-cd /tmp
-wget https://github.com/Automattic/simplenote-electron/releases/download/v1.0.8/simplenote-1.0.8.deb
-sudo gedbi simplenote-1.0.8.deb
-
-echo "Go ahead and install Chrome"
-read -p "Press [Enter] key to move onto installing Simplenote"
+#Install Anaconda (this should launche the internet and start the download)
+sensible-browser https://www.anaconda.com/download/#linux
